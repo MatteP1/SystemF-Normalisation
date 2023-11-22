ca31c71d1adf107dea28c319575048ace0bc524e
RusTy <mathiasp9999@gmail.com>
2023-11-22 18:13:11 +0100

    cleaned up files.

--- Maps.v
+++ Maps.v
@@ -1,33 +1,6 @@
 (** * Maps: Total and Partial Maps *)
 
-(** _Maps_ (or _dictionaries_) are ubiquitous data structures both in
-    ordinary programming and in the theory of programming languages;
-    we're going to need them in many places in the coming chapters.
-
-    They also make a nice case study using ideas we've seen in
-    previous chapters, including building data structures out of
-    higher-order functions (from [Basics] and [Poly]) and the use of
-    reflection to streamline proofs (from [IndProp]).
-
-    We'll define two flavors of maps: _total_ maps, which include a
-    "default" element to be returned when a key being looked up
-    doesn't exist, and _partial_ maps, which instead return an
-    [option] to indicate success or failure.  The latter is defined in
-    terms of the former, using [None] as the default element. *)
-
-(* ################################################################# *)
-(** * The Coq Standard Library *)
-
-(** One small digression before we begin...
-
-    Unlike the chapters we have seen so far, this one does not
-    [Require Import] the chapter before it (nor, transitively, all the
-    earlier chapters).  Instead, in this chapter and from now, on
-    we're going to import the definitions and theorems we need
-    directly from Coq's standard library stuff.  You should not notice
-    much difference, though, because we've been careful to name our
-    own definitions and theorems the same as their counterparts in the
-    standard library, wherever they overlap. *)
+(* NOTE: THIS FILE IS TAKEN FROM PROGRAMMING LANGUAGE FOUNDATIONS *)
 
 From Coq Require Import Arith.Arith.
 From Coq Require Import Bool.Bool.
@@ -36,127 +9,41 @@ From Coq Require Import Logic.FunctionalExtensionality.
 From Coq Require Import Lists.List.
 Import ListNotations.
 
-(** Documentation for the standard library can be found at
-    https://coq.inria.fr/library/.
-
-    The [Search] command is a good way to look for theorems involving
-    objects of specific types. See [Lists] for a reminder of how
-    to use it. *)
-
-(** If you want to find out how or where a notation is defined, the
-    [Locate] command is useful.  For example, where is the natural
-    addition operation defined in the standard library? *)
-
-Locate "+".
-
-(** (There are several uses of the [+] notation, but only one for
-    naturals.) *)
-
-Print Init.Nat.add.
-
-(** We'll see some more uses of [Locate] in the [Imp] chapter. *)
-
-(* ################################################################# *)
-(** * Identifiers *)
-
-(** First, we need a type for the keys that we will use to index into
-    our maps.  In [Lists.v] we introduced a fresh type [id] for a
-    similar purpose; here and for the rest of _Software Foundations_
-    we will use the [string] type from Coq's standard library. *)
-
-(** To compare strings, we use the function [eqb] from the [String]
-    module in the standard library. *)
-
-Check String.eqb_refl :
-  forall x : string, (x =? x)%string = true.
-
-(** We will often use a few basic properties of string equality... *)
-Check String.eqb_eq :
-  forall n m : string, (n =? m)%string = true <-> n = m.
-Check String.eqb_neq :
-  forall n m : string, (n =? m)%string = false <-> n <> m.
-Check String.eqb_spec :
-  forall x y : string, reflect (x = y) (String.eqb x y).
-
 (* ################################################################# *)
 (** * Total Maps *)
 
-(** Our main job in this chapter will be to build a definition of
-    partial maps that is similar in behavior to the one we saw in the
-    [Lists] chapter, plus accompanying lemmas about its behavior.
-
-    This time around, though, we're going to use _functions_, rather
-    than lists of key-value pairs, to build maps.  The advantage of
-    this representation is that it offers a more "extensional" view of
-    maps: two maps that respond to queries in the same way will be
-    represented as exactly the same function, rather than just as
-    "equivalent" list structures.  This, in turn, simplifies proofs
-    that use maps. *)
-
-(** We build up to partial maps in two steps.  First, we define a type
-    of _total maps_ that return a default value when we look up a key
-    that is not present in the map. *)
-
 Definition total_map (A : Type) := string -> A.
-
-(** Intuitively, a total map over an element type [A] is just a
-    function that can be used to look up [string]s, yielding [A]s. *)
-
-(** The function [t_empty] yields an empty total map, given a default
-    element; this map always returns the default element when applied
-    to any string. *)
 
 Definition t_empty {A : Type} (v : A) : total_map A :=
   (fun _ => v).
-
-(** More interesting is the map-updating function, which (as always)
-    takes a map [m], a key [x], and a value [v] and returns a new map
-    that takes [x] to [v] and takes every other key to whatever [m]
-    does.  The novelty here is that we achieve this effect by wrapping
-    a new function around the old one. *)
 
 Definition t_update {A : Type} (m : total_map A)
                     (x : string) (v : A) :=
   fun x' => if String.eqb x x' then v else m x'.
 
-(** This definition is a nice example of higher-order programming:
-    [t_update] takes a _function_ [m] and yields a new function
-    [fun x' => ...] that behaves like the desired map. *)
 
-(** For example, we can build a map taking [string]s to [bool]s, where
-    ["foo"] and ["bar"] are mapped to [true] and every other key is
-    mapped to [false], like this: *)
-
+(* Example *)
 Definition examplemap :=
   t_update (t_update (t_empty false) "foo" true)
            "bar" true.
 
-(** Next, let's introduce some notations to facilitate working with
-    maps. *)
 
-(** First, we use the following notation to represent an empty total
-    map with a default value. *)
+(* Notation *)
 Notation "'_' '!->' v" := (t_empty v)
   (at level 100, right associativity).
 
 Example example_empty := (_ !-> false).
 
-(** We next introduce a convenient notation for extending an existing
-    map with a new binding. *)
 Notation "x '!->' v ';' m" := (t_update m x v)
                               (at level 100, v at next level, right associativity).
 
-(** The [examplemap] above can now be defined as follows: *)
+(* Examples *)
 
 Definition examplemap' :=
   ( "bar" !-> true;
     "foo" !-> true;
     _     !-> false
   ).
-
-(** This completes the definition of total maps.  Note that we
-    don't need to define a [find] operation on this representation of
-    maps because it is just function application! *)
 
 Example update_example1 : examplemap' "baz" = false.
 Proof. reflexivity. Qed.
@@ -170,15 +57,6 @@ Proof. reflexivity. Qed.
 Example update_example4 : examplemap' "bar" = true.
 Proof. reflexivity. Qed.
 
-(** When we use maps in later chapters, we'll need several fundamental
-    facts about how they behave. *)
-
-(** Even if you don't bother to work the following exercises,
-    make sure you thoroughly understand the statements of the
-    lemmas! *)
-
-(** (Some of the proofs require the functional extensionality axiom,
-    which was discussed in the [Logic] chapter.) *)
 
 (** **** Exercise: 1 star, standard, optional (t_apply_empty)
 
